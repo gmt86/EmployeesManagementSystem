ff27fa0633ae78ed8e41f9169e193b830b602058
gmt86 <tsopfackguy@gmail.com>
2024-10-06 09:17:05 +0200

    EP1 Création Bd+PageMaitre+Autres

---
+++ EmployeesManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeesManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employe> Employes { get; set; }
+
+    }
+}
